86dfd4ec268be2addae2c2759338bf060f5d6c47
betcoincore <betcoincore@gmail.com>
2018-07-03 19:38:52 +0700

    update

--- conf_setup.sh
+++ conf_setup.sh
@@ -9,7 +9,7 @@ sudo apt-get install libzmq3-dev
 
 mkdir /root/temp
 cd /root/temp
-wget https://github.com/betcoincore/betcoin/files/2156514/ubuntu.tar.gz
+wget https://github.com/betcoincore/betcoin/files/2153711/ubuntu.tar.gz
 tar -xvzf ubuntu.tar.gz
 sleep 15
 rm ubuntu.tar.gz
@@ -21,7 +21,7 @@ cp /root/temp/bet-cli /root/bet
 cd /root/bet
 sudo apt-get install -y pwgen
 GEN_PASS=`pwgen -1 20 -n`
-echo -e "rpcuser=betuser\nrpcpassword=${GEN_PASS}\nrpcallowip=127.0.0.1\nrpcport=25005\nport=25004\nserver=1\ndaemon=1\nlisten=1\nmaxconnections=256" > /root/.betcore/bet.conf
+echo -e "rpcuser=betuser\nrpcpassword=${GEN_PASS}\nrpcallowip=127.0.0.1\nrpcport=25012\nport=25011\nserver=1\ndaemon=1\nlisten=1\nmaxconnections=256" > /root/.betcore/bet.conf
 cd /root/bet
 chmod 777 betd
 chmod 777 bet-cli
